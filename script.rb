8dcc3c5657d10f66765b1e182321bb90db059f33
Nick Yaremovich <nickyaremovich@gmail.com>
2026-05-18 16:31:49 -0600

    add lowercase only functionality

--- script.rb
+++ script.rb
@@ -1,9 +1,14 @@
 # hello
-def caesar_cipher(string)
+def caesar_cipher(string, shift)
   string.chars.map do |char|
-    char.ord
-  end
+    if char.match?(/[a-z]/)
+      start = "a".ord #97 
+      position = char.ord - start # for a it would be 97-97=0
+      new_position = (position + shift) % 26 # actual shift occurs here / modulo 26 to wrap z -> a
+      (new_position + start).chr #add back the original ASCII value and convert to char
+    end
+  end.join
 end
 
-puts caesar_cipher("Hello, World!")
+puts caesar_cipher("abc", 3)
 
