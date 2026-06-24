9cc1dc14d484472e2fe2979aeb90d601d88a6c7b
Nick Yaremovich <nickyaremovich@gmail.com>
2026-06-23 18:43:57 -0600

    use rubocop for first time and correct offences

--- script.rb
+++ script.rb
@@ -1,13 +1,15 @@
+# frozen_string_literal: true
+
 # hello
 def caesar_cipher(string, shift)
   string.chars.map do |char|
     if char.match?(/[a-z]/)
-      start = "a".ord #97 
+      start = 'a'.ord # 97
       position = char.ord - start # for a it would be 97-97=0
       new_position = (position + shift) % 26 # actual shift occurs here / modulo 26 to wrap z -> a
-      (new_position + start).chr #add back the original ASCII value and convert to char
+      (new_position + start).chr # add back the original ASCII value and convert to char
     elsif char.match?(/[A-Z]/)
-      start = "A".ord
+      start = 'A'.ord
       position = char.ord - start
       new_position = (position + shift) % 26
       (new_position + start).chr
@@ -17,5 +19,4 @@ def caesar_cipher(string, shift)
   end.join
 end
 
-puts caesar_cipher("abc", 3)
-
+puts caesar_cipher('Hello, World!', 3)
